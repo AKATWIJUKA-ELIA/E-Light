65ac02e1ad08734701ca1d9ea6e4bf209b8fbe9d
Elia Light <141272836+AKATWIJUKA-ELIA@users.noreply.github.com>
2025-04-21 13:29:40 +0300

    fix

--- src/components/DropDownMenu/page.tsx
+++ src/components/DropDownMenu/page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {  useState } from 'react';
 import Link from "next/link";
 import { BiX } from 'react-icons/bi';
 import useGetCategories from '@/hooks/useGetCategories';
