27e1db7a6e7ad0a3a58c54b6d8c35e51667e4782
Elia Light <141272836+AKATWIJUKA-ELIA@users.noreply.github.com>
2025-04-24 09:37:49 +0300

    Protect Administrator Route

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,33 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtected = createRouteMatcher([
         '/post(.*)'
 ])
+const isRoleProtected = createRouteMatcher([
+        '/administrator(.*)'
+])
 export default clerkMiddleware(async(auth, req)=>{
         if(isProtected(req)){
                 await auth.protect()
         }
+        if (isRoleProtected(req)) {
+                const { userId, sessionClaims } = await auth();
+            
+                if (!userId) {
+                  await auth.protect();
+                }
+
+                // Define the type for publicMetadata
+                type PublicMetadata = {
+                  role?: string;
+                };
+
+                const publicMetadata = sessionClaims?.publicMetadata as PublicMetadata;
+                const userRole = publicMetadata?.role;
+            
+                if (userRole !== "admin") {
+                  // Redirect or throw custom error
+                  return new Response("Unauthorized", { status: 403 });
+                }
+              }
 });
 
 export const config = {
