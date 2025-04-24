dbf2a7cac9ca938a48189256e45f234a5e8c64b8
Elia Light <141272836+AKATWIJUKA-ELIA@users.noreply.github.com>
2025-04-24 16:12:56 +0300

    New Hero

--- src/components/FirstHero/page.tsx
+++ src/components/FirstHero/page.tsx
@@ -43,15 +43,15 @@ const FisrtHero = () => {
 
         
   return (
-        <div className='bg-pink-200 mb-10 ' >
+        <div className='bg-gradient-to-r from-indigo-100 via-purple-100 to-gold mb-10 ' >
 <div>
-<h1></h1>
+<h1 className='font-bold'>Categories</h1>
 </div>
-<div className=' hidden md:grid grid-cols-1 md:grid-cols-4'>
+<div className=' hidden md:grid grid-cols-1 md:grid-cols-1'>
 <Carousel opts={{align: "start",loop: true,}} plugins={[carousel]} className="  w-full">
         <CarouselContent className=''>
   {products.map((product, index) => (
-    <CarouselItem key={index}>
+    <CarouselItem key={index} className='basis-[300px] shrink-0'>
       <div className="p-1">
         <Card className="h-auto bg-transparent">
           <Link href={`/category/${product.product_cartegory}`} >
@@ -81,104 +81,7 @@ const FisrtHero = () => {
         <CarouselNext />
         </Carousel>
 
-        <Carousel opts={{align: "start",loop: true,}} plugins={[carousel1]} className="  w-full hidden md:flex">
-        <CarouselContent>
-  {products.map((product, index) => (
-    <CarouselItem key={index}>
-      <div className="p-1">
-        <Card className="h-auto">
-        <Link href={`/category/${product.product_cartegory}`} >
-          <CardContent className="relative  bg-transparent flex items-center justify-center p-6 h-64 overflow-hidden rounded-lg">
-            {/* Image */}
-            <Image
-              src={product.product_image}
-        //       height={100}
-        //       width={450}
-              alt={product.product_name}
-             fill
-             className='object-cover w-full h-full'
-            />
 
-            {/* Text Overlay */}
-            <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black/40 text-white text-xl font-semibold p-4">
-              {product.product_cartegory}
-            </div>
-          </CardContent>
-          </Link>
-        </Card>
-      </div>
-    </CarouselItem>
-  ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-        </Carousel>
-
-        <Carousel opts={{align: "start",loop: true,}} plugins={[carousel2]} className="  w-full hidden md:flex">
-        <CarouselContent>
-  {products.map((product, index) => (
-    <CarouselItem key={index}>
-      <div className="p-1">
-        <Card className="h-auto">
-        <Link href={`/category/${product.product_cartegory}`} >
-          <CardContent className="relative  bg-transparent flex items-center justify-center p-6 h-64 overflow-hidden rounded-lg">
-            {/* Image */}
-            <Image
-              src={product.product_image}
-        //       height={100}
-        //       width={450}
-              alt={product.product_name}
-             fill
-             className='object-cover w-full h-full'
-            />
-
-            {/* Text Overlay */}
-            <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black/40 text-white text-xl font-semibold p-4">
-              {product.product_cartegory}
-            </div>
-          </CardContent>
-          </Link>
-        </Card>
-      </div>
-    </CarouselItem>
-  ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-        </Carousel>
-
-        <Carousel opts={{align: "start",loop: true,}} plugins={[carousel3]} className="  w-full hidden md:flex">
-        <CarouselContent>
-  {products.map((product, index) => (
-    <CarouselItem key={index}>
-      <div className="p-1">
-        <Card className="h-auto">
-        <Link href={`/category/${product.product_cartegory}`} >
-          <CardContent className="relative  bg-transparent flex items-center justify-center p-6 h-64 overflow-hidden rounded-lg">
-            {/* Image */}
-            <Image
-              src={product.product_image}
-        //       height={100}
-        //       width={450}
-              alt={product.product_name}
-             fill
-             className='object-cover w-full h-full'
-            />
-
-            {/* Text Overlay */}
-            <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black/40 text-white text-xl font-semibold p-4">
-              {product.product_cartegory}
-            </div>
-          </CardContent>
-          </Link>
-        </Card>
-      </div>
-    </CarouselItem>
-  ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-        </Carousel>
 </div>
         </div>
   )
--- src/components/MainHero/page.tsx
+++ src/components/MainHero/page.tsx
@@ -11,10 +11,11 @@ import {
 } from "@/components/ui/carousel"
 import Autoplay from "embla-carousel-autoplay"
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface Product {
         approved: boolean;
-         product_category: string;
+         product_cartegory: string;
          product_condition: string;
          product_description: string;
          product_image: string;
@@ -26,8 +27,27 @@ interface Product {
        }
 
 const MainHero = () => {
+        const carousel1 = Autoplay({ delay: 9000})
         const carousel = Autoplay({ delay: 10000})
+        const images = [
+                {
+                name:"Heror",
+                src:"/images/Heror.png",
+                overlay:"April Sales Here 😊 "
+                },
+                {
+                        name:"Hero1",
+                        src:"/images/Hero.jpg",
+                        overlay:"Grab April Discounts While Offers Last😁🤗"
+                },
+                {
+                        name:"Heror",
+                        src:"/images/Hero3.jpg",
+                        overlay:"Different Categories For Grabs😂😮"
+                },
+            
 
+        ]
                 const [products, setproducts] = useState<Product[]>([]);
                         const product = useQuery(api.products.getProducts)
                         
@@ -39,29 +59,27 @@ const MainHero = () => {
 
         
   return (
-        <div className='bg-pink-200   mt-32 ' >
+        <div className= ' bg-pink-200   mt-32 grid grid-cols-1  '  >
 
-        <div className='grid grid-cols-4 ml-32' >
-        <Carousel opts={{align: "start",loop: true}} plugins={[carousel]} className="">
-        <CarouselContent className=''>
-  {products.map((product, index) => (
-    <CarouselItem key={index} className="basis-[300px] shrink-0">
-      <div className="p-1">
+        <Carousel opts={{align: "start",loop: true}} plugins={[carousel1]} className="h-full">
+        <CarouselContent className='h-full'>
+  {images.map((image, index) => (
+    <CarouselItem key={index} className="h-full">
+      <div className="h-full  ">
         <Card className="h-auto bg-transparent w-full">
-          <CardContent className="relative  bg-transparent flex items-center justify-center p-6 h-48 overflow-hidden w-full">
+          <CardContent className="relative  bg-transparent flex  items-center justify-center  h-80 overflow-hidden w-full">
             {/* Image */}
+        
             <Image
-              src={product.product_image}
+              src={image.src}
         //       height={100}
         //       width={450}
-              alt={product.product_name}
+              alt={image.name}
              fill
-             className='object-cover w-full h-full'
+             className='object-cover w-full h-full  '
             />
-
-            {/* Text Overlay */}
-            <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black/40 text-white text-xl font-semibold p-4">
-              {product.product_name}
+            <div className="absolute top-7 md:left-16 flex items-center justify-center bg-black/40 text-white text-3xl font-semibold p-4">
+              {image.overlay}
             </div>
           </CardContent>
         </Card>
@@ -72,8 +90,41 @@ const MainHero = () => {
         <CarouselPrevious />
         <CarouselNext />
         </Carousel>
-        </div>
 
+        <Carousel opts={{align: "start",loop: true}} plugins={[carousel]} className="absolute w-[60%] md:w-[65%] md:left-16  mt-40 md:mt-36  flex items-center justify-center bg-black/40 text-white text-xl font-semibold md:p-2">
+        <CarouselContent className=''>
+  {products.map((product, index) => (
+    <CarouselItem key={index} className=" basis-[200px] md:basis-[300px] shrink-0">
+      <div className="p-1">
+        <Card className="h-auto bg-transparent w-full">
+          <CardContent className="relative  bg-transparent flex rounded-lg items-center justify-center p-6 h-36 overflow-hidden w-full">
+            {/* Image */}
+            <Link href={`/category/${product.product_cartegory}`} >
+            <Image
+              src={product.product_image}
+        //       height={100}
+        //       width={450}
+              alt={product.product_name}
+             fill
+             className='object-cover w-full h-full rounded-lg '
+            />
+            
+
+            {/* Text Overlay */}
+            <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black/40 text-white text-xl font-semibold p-4">
+              {product.product_name}
+            </div>
+            </Link>
+          </CardContent>
+        </Card>
+      </div>
+    </CarouselItem>
+  ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+        </Carousel>
+ 
         </div>
   )
 }
