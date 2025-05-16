2cf3ca34cf63adcfbfcc7092a3646d9a8a3f0d50
Elia Light <141272836+AKATWIJUKA-ELIA@users.noreply.github.com>
2025-05-16 13:59:01 +0300

    Schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -16,13 +16,21 @@ export default defineSchema({
     user_id: v.string(),
   }),
   cartegories: defineTable({ cartegory: v.string() }),
+
   customers: defineTable({
-    address: v.string(),
+    fullName: v.string(),
     email: v.string(),
-    password: v.string(),
-    phone_number: v.string(),
-    username: v.string(),
+    passwordHash: v.string(),
+    phoneNumber: v.optional(v.string()),
+    profilePicture: v.optional(v.string()),
+    isVerified: v.boolean(),
+    role: v.string(),
+    reset_token: v.optional(v.string()),
+    reset_token_expires:v.number(),
+    updatedAt: v.number(),
+    lastLogin: v.optional(v.number()),
   }),
+
   orders: defineTable({
     order_status: v.string(),
     product_cartegory: v.string(),
--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -4,8 +4,10 @@ import React from 'react'
 
 const Signin = () => {
   return (
-    <div className="flex justify-center" >
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10" >
+        <div className="w-full max-w-sm md:max-w-3xl">
         <SignIn />
+      </div>
     </div>
   )
 }
