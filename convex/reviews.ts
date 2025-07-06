ce147146758456868dcf765010d99e8a3ff01fe5
Elia Light <141272836+AKATWIJUKA-ELIA@users.noreply.github.com>
2025-07-06 14:38:12 +0300

    user reviews only once per product

--- convex/reviews.ts
+++ convex/reviews.ts
@@ -15,6 +15,13 @@ export const createReview = mutation({
   },
   handler: async (ctx, args) => {
         try {
+                const existing = ctx.db.query("reviews").filter((q)=>q.and(
+                        q.eq("product_id",args.product_id),
+                        q.eq("reviewer_id",args.reviewer_id)
+                ) )
+                if(existing){
+                        return { success: false, message: "Only one review per product is allowed" };
+                }
     await ctx.db.insert("reviews", {
       ...args,
     });
