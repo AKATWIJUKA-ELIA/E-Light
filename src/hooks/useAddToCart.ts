6148d7dcddbdd2aa8e6f723a76c82784d40bb8a5
Elia Light <141272836+AKATWIJUKA-ELIA@users.noreply.github.com>
2025-07-21 09:33:20 +0300

    fix 7654321

--- src/hooks/useAddToCart.ts
+++ src/hooks/useAddToCart.ts
@@ -2,13 +2,14 @@ import { useAppDispatch } from "@/hooks";
 import { addToCart } from "@/store/cart";
 import useCart from '@/hooks/useCart';
 import useRecordInteraction from "./useRecordInteraction";
+import { Id } from "../../convex/_generated/dataModel";
 
  
 const useAddToCart = ()=>{
          const { CreateCart } = useCart();
          const { record } = useRecordInteraction();
         const dispatch = useAppDispatch();
-                return (product:{_id:string})=> {dispatch(addToCart({
+                return (product:{_id:Id<"products">})=> {dispatch(addToCart({
                         product_id: product._id,
                         quantity:1
                 }))
