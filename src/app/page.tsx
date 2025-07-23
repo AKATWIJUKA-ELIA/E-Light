24d1af00e92ebde4cfc844b6209114b964293e79
Elia Light <141272836+AKATWIJUKA-ELIA@users.noreply.github.com>
2025-07-23 12:33:47 +0300

    loader update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,39 @@
-"use client"
-import FisrtHero from "@/components/FirstHero/page";
-import SecondHero from "@/components/SecondHero/page";
-import Main from "@/components/Main/main";
-import { Provider } from "react-redux"
-import { PersistGate } from "redux-persist/integration/react";
-import {store,persistor} from "@/store/store"
-import MainHero from "@/components/MainHero/page";
-import TopRatings from "@/components/TopRatings/main";
-export default function Home() {
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useData } from "./DataContext";
+import Loader from "@/components/Loader/loader";
+import useGetSponsored from '@/hooks/useGetSponsored';
+
+
+export default function Page() {
+  const router = useRouter();
+  const {data} = useData();
+   const { sponsored: sponsored } = useGetSponsored();
+
+  useEffect(() => {
+    if (!data|| data.Products.product.length === 0 
+        || data.Categories.categories.length === 0
+        || !sponsored || sponsored.length === 0) {
+      return
+    }else{
+        router.push("/home"); 
+    }
+    
+  }, [data, router]);
+
+  // Show loading state while redirecting
   return (
-        <div className="backdrop-blur-md"
-        // style={{ backgroundImage: `url("images/wallp.jpg")`,
-        //                 backgroundSize: 'cover',
-        //                 backgroundPosition: 'center' }}
-        >
-                <Provider store={store}>
-                <PersistGate persistor={persistor}>
-                        <MainHero />
-                        <SecondHero />
-                        <FisrtHero />
-                        <TopRatings />
-                        <Main />
-                </PersistGate>
- </Provider>
+    <div className="relative flex  items-center justify-center bg-white/10 opacity-50 min-h-screen"
+    style={{ backgroundImage: `url("images/wallp.jpg")`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center' }}
+    >
+        <div className="absolute inset-0 bg-transparent bg-opacity-15 backdrop-blur-lg">
+                <Loader />
         </div>
+      
+    </div>
   );
 }
---
+++ src/app/sessionchecker.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { useEffect } from "react";
+import useLogout from "@/hooks/useLogout";
+
+export default function SessionChecker() {
+          const logout = useLogout();
+  useEffect(() => {
+    const checkSession = async () => {
+      try {
+        const res = await fetch("/api/checksession", { cache: "no-store" });
+        const data = await res.json();
+
+        if (!data.valid) {
+          console.log("Session invalid. Clearing localStorage.");
+          logout();
+          // Optionally redirect to login page
+          window.location.href = "/sign-in";
+        }
+        
+      } catch (error) {
+        console.error("Error checking session validity", error);
+        logout();
+        window.location.href = "/sign-in"; // Redirect on error
+      }
+    };
+
+    checkSession();
+  }, []);
+
+  return null; // It's just a checker component
+}
