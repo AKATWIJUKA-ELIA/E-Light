4f6329c2c06557e7a9f5f1abe011e3bd7b201696
Elia Light <141272836+AKATWIJUKA-ELIA@users.noreply.github.com>
2025-07-27 17:00:53 +0300

    responsive user profile Sidebar

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { AppSidebar } from "@/components/app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider,SidebarTrigger  } from "@/components/ui/sidebar";
 import { useState } from "react";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 import { BoostProvider } from "../BoostContext";
@@ -20,13 +20,17 @@ export default function ProfileLayout({
         <BoostProvider>
         <SidebarProvider open={isopen} onOpenChange={setisOpen}  >
         <AppSidebar   />
-                <button
-                  className={`fixed hidden md:flex  top-24 ${isopen? "left-60":"left-4"} z-50 p-2 bg-blue-400  text-white rounded-full shadow-lg shadow-black/50 border  hover:bg-blue-600 dark:bg-white dark:text-black transition-colors`}
+        
+                {/* <button
+                  className={`fixed hidden md:flex  top-24 
+                        ${isopen? "left-60":"left-4"}
+                         z-50 p-2 bg-blue-400  text-white rounded-full shadow-lg shadow-black/50 border  hover:bg-blue-600 dark:bg-white dark:text-black transition-colors`}
                   onClick={toggleSidebar}
                 >
                   {isopen ? <FaAngleLeft /> : <FaAngleRight />}
-                </button>
-      <main className="flex-1 overflow-y-auto">
+                </button> */}
+      <main className="flex-1 overflow-y-auto relative ">
+        <SidebarTrigger className="absolute z-50 bg-dark text-white dark:bg-blue-800 top-[13%] " />
         {children}
       </main>
       </SidebarProvider>
