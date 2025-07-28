271b7252fe627f0e904ffe8aa5bd35f20316706e
Elia Light <141272836+AKATWIJUKA-ELIA@users.noreply.github.com>
2025-07-28 18:10:35 +0300

    fix no used vars

--- src/app/sudo/layout.tsx
+++ src/app/sudo/layout.tsx
@@ -1,23 +1,18 @@
 "use client";
 import { AppSidebar } from "@/sudoComponents/app-sidebar";
 import { SidebarProvider,SidebarTrigger } from "@/components/ui/sidebar";
-import { useState } from "react";
-import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 
 
 export default function ProfileLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {
-         const [isopen, setisOpen] = useState(true);   
-        const toggleSidebar = () => {
-                  setisOpen(prev => !prev);
-                };
+}) {           
+
   return (
     <div className="flex flex-col h-screen backdrop-blur-2xl">
         
-        <SidebarProvider open={isopen} onOpenChange={setisOpen}  >
+        <SidebarProvider  >
         <AppSidebar   />
                 {/* <button
                   className={`fixed hidden md:flex  top-24 ${isopen? "left-60":"left-4"} z-50 p-2 bg-blue-400  text-white rounded-full shadow-lg shadow-black/50 border  hover:bg-blue-600 dark:bg-white dark:text-black transition-colors`}
